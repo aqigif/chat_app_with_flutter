0299c1f774c6cfcb703d51c9cd0d27cec3d14a0d
aqil gifari <aqilgifari0@gmail.com>
2019-10-31 17:09:21 +0700

    appBar: initialize

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
+        accentColor: Colors.white
       ),
       home: HomeScreen(),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui_starter/widgets/category_selector.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,7 +10,42 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-    );
+        backgroundColor: Theme.of(context).primaryColor,
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            iconSize: 28.0,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+          title: Text('Chat',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              )),
+          elevation: 0.0,
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.search),
+                iconSize: 28.0,
+                color: Colors.white,
+                onPressed: () {})
+          ],
+        ),
+        body: Column(
+          children: <Widget>[
+            CategorySelector(),
+            Expanded(
+              child: Container(
+                height: 500.0,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).accentColor,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30.0),
+                        topRight: Radius.circular(30.0))),
+              ),
+            )
+          ],
+        ));
   }
 }
